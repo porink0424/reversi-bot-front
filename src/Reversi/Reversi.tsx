7c33e1053443f030b64eb1bae467d99bdb74269a
Daichi Kato <daichi5967@docomo.ne.jp>
2023-04-23 01:32:35 +0900

    Update setTimeout delay

--- src/Reversi/Reversi.tsx
+++ src/Reversi/Reversi.tsx
@@ -57,7 +57,7 @@ document.addEventListener("mousemove", (event: MouseEvent) => {
  */
 setTimeout(() => {
   deleteDisc([3, 3], discs, scene);
-}, 1000);
+}, 5000);
 
 function Reversi() {
   return <div></div>;
