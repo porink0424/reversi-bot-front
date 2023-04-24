0b51d3c824f0eda3a68761c1d4da50d17d9094c2
Daichi Kato <daichi5967@docomo.ne.jp>
2023-04-25 00:50:53 +0900

    Add eslintrc

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// import init, { greet } from "./pkg/reversi_bot";
-// init().then(greet);
+// import init, { Board, calc_legal_places } from "./pkg/reversi_bot";
+// import { COLOR } from "./pkg/reversi_bot";
+// init().then(() => {
+//   const board = new Board();
+//   board.set(
+//     BigInt(0x0000000810000000),
+//     BigInt(0x0000001008000000),
+//     4,
+//     COLOR.BLACK
+//   );
+//   console.log(calc_legal_places(board).toString(16));
+// });
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
