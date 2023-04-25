6ccf9fa82bf7601aa1121edd405a9b74b26849b0
Daichi Kato <daichi5967@docomo.ne.jp>
2023-04-25 14:54:02 +0900

    Implement convert function between int <-> placeArray

--- src/Reversi/utils.ts
+++ src/Reversi/utils.ts
@@ -8,3 +8,20 @@ export const reversiPositionToThreePosition = (
   const y = LINE_COUNT - position[1] - 1 - (BOARD_SIZE / 2 - CELL_SIZE / 2);
   return { x, y };
 };
+
+export const bigIntToPlaces = (bigInt: bigint): ReversiPosition[] => {
+  const placesArray: ReversiPosition[] = [];
+  for (let i = 0; i < 64; i += 1) {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+    if (bigInt & (BigInt(0x8000000000000000) >> BigInt(i))) {
+      placesArray.push([x, y]);
+    }
+  }
+  return placesArray;
+};
+
+export const placeToBigInt = (place: ReversiPosition): bigint => {
+  const [x, y] = place;
+  return BigInt(0x8000000000000000) >> BigInt(x + y * 8);
+};
